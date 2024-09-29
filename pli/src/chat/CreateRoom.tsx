7ea42931c0975ea45699b92a6983f582032f4877
Rosny Chum <chum_r@etna-alternance.net>
2024-09-29 22:31:18 +0200

    feat(room): added CreateRoom component
    - Added a component/page to create a room with an id, and redirect user
      to this room

---
+++ pli/src/chat/CreateRoom.tsx
@@ -0,0 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+
+const CreateRoom = () => {
+    const navigate = useNavigate();
+
+    function create() {
+        const id = uuidv4();
+        navigate(`/room/${id}`);
+    }
+
+    return (
+        <button onClick={create}>Create room</button>
+    );
+}
+
+export default CreateRoom;
