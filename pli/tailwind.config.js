3396ce86f42d485533565e4240d4da88e275f62b
delaut_a <delaut_a@etna-alternance.net>
2024-10-28 21:54:02 +0100

    Feat/room interface

--- pli/tailwind.config.js
+++ pli/tailwind.config.js
@@ -5,57 +5,39 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      transparent: colors.transparent,
-      current: colors.current,
-      fuchsia: colors.fuchsia,
-      'blue': {
-        '50': '#eef4ff',
-        '100': '#e0ebff',
-        '200': '#c7dafe',
-        '300': '#a5c1fc',
-        '400': '#819df8',
-        '500': '#6d83f2',
-        '600': '#4654e5',
-        '700': '#3842ca',
-        '800': '#303aa3',
-        '900': '#2e3781',
-        '950': '#1b1f4b',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'deep-blue': "#546bd6",
       },
-      'deep-blue': {
-        '50': '#f2f4fc',
-        '100': '#e1e6f8',
-        '200': '#c9d3f4',
-        '300': '#a4b7ec',
-        '400': '#7993e1',
-        '500': '#546bd6',
-        '600': '#4554cb',
-        '700': '#3c44b9',
-        '800': '#363997',
-        '900': '#303478',
-        '950': '#21224a',
+      width: {
+        '112': '28rem', // 448px
+        '120': '30rem', // 480px
+        '136': '34rem', // 544px
+        '152': '38rem', // 608px
+        '168': '42rem', // 672px
+        '176': '44rem', // 704px
+        '192': '48rem', // 768px
+        '224': '56rem', // 896px
+        '256': '64rem', // 1024px
+        '288': '72rem', // 1152px
+        '320': '80rem', // 1280px
       },
-      'red': {
-        '50': '#fef2f2',
-        '100': '#fde3e3',
-        '200': '#fccccc',
-        '300': '#f9a8a8',
-        '400': '#f26161',
-        '500': '#ea4949',
-        '600': '#d62c2c',
-        '700': '#b42121',
-        '800': '#951f1f',
-        '900': '#7c2020',
-        '950': '#430c0c',
+      height: {
+        '112': '28rem', // 448px
+        '120': '30rem', // 480px
+        '136': '34rem', // 544px
+        '152': '38rem', // 608px
+        '168': '42rem', // 672px
+        '176': '44rem', // 704px
+        '192': '48rem', // 768px
+        '224': '56rem', // 896px
+        '256': '64rem', // 1024px
+        '288': '72rem', // 1152px
+        '320': '80rem', // 1280px
       },
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      emerald: colors.emerald,
-      indigo: colors.indigo,
-      yellow: colors.yellow,
     },
-    extend: {},
   },
   plugins: []
 }
