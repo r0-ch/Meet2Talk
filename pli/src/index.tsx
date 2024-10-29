b936e3750f442553251c59e6471fae64a831445d
chen_a <chen_a@etna-alternance.net>
2024-10-29 22:07:42 +0100

    Remove unused chat components and update routing; enhance styling for better UI consistency

--- pli/src/index.tsx
+++ pli/src/index.tsx
@@ -5,11 +5,7 @@ import "./index.css";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import CreateRoom from './chat/CreateRoom';
-import Room from './chat/Room';
-import RoomInterface from './chat/RoomInterface';
 import Home from './routes/home';
-import Test from './chat/Test';
 import ChatRoom from './chat/ChatRoom';
 
 const root = ReactDOM.createRoot(
@@ -22,27 +18,9 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: "/room/create",
-    element: <CreateRoom />,
-  },
-  {
     path: "/room/interface/:roomId",
-    // element: <RoomInterface />,
-    // element: <Test />,
     element: <ChatRoom />,
   },
-  {
-    path: "/room/:id",
-    element: <Room />,
-  }
-  // {
-  //   path: "/room",
-  //   element: <RoomCreate/>,
-  // },
-  // {
-  //   path: "/room/:roomId";
-  //   element: <Room/>,
-  // }
 ]);
 
 root.render(
