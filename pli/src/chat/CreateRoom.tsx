04f298b21829e531518747e48aa0c912d398fc46
chen_a <chen_a@etna-alternance.net>
2024-10-29 16:36:41 +0100

    Halloween version

--- pli/src/chat/CreateRoom.tsx
+++ pli/src/chat/CreateRoom.tsx
@@ -15,15 +15,22 @@ const CreateRoom = () => {
 
     const handleCreateRoom = () => {
         const roomId = generateRoomId();  // Génère un roomId aléatoire
-
         // Naviguer vers la route avec roomId visible et username dans le state
         navigate(`/room/interface/${roomId}`, { state: { username } });
     };
 
     return (
-        <div>
-            <h1>Bonjour {username}</h1>
-            <button onClick={handleCreateRoom}>Create Room</button>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-gray-200">
+            <h1 className="text-4xl font-bold mb-4 animate-fade-in">Bonjour {username}</h1>
+            <p className="text-lg text-gray-400 mb-8 text-center animate-fade-in">
+                Cliquez sur le bouton ci-dessous pour créer une salle d'attente ou rejoindre une salle existante.
+            </p>
+            <button 
+                onClick={handleCreateRoom} 
+                className="px-6 py-3 bg-gray-700 hover:bg-gray-600 text-gray-200 font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+            >
+                Create Room
+            </button>
         </div>
     );
 };
