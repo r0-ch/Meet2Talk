ba20057f7ffad3fd5fa10faa0b57f05aedf4754b
delaut_a <delaut_a@etna-alternance.net>
2024-10-30 21:50:08 +0100

    update chat

--- pli/src/chat/ChatRoom.tsx
+++ pli/src/chat/ChatRoom.tsx
@@ -66,10 +66,8 @@ const ChatRoom = () => {
     };
 
     useEffect(() => {
-        const socket = io(`${process.env.REACT_APP_BACKEND}`, {
-            path: '/socket.io',
+        const socket = io(`${process.env.REACT_APP_BACKEND}`, { 
             withCredentials: true,
-            // tryAllTransports: true,
         });
         console.log(`api: ${process.env.REACT_APP_BACKEND}`);
         socketRef.current = socket;
@@ -649,10 +647,10 @@ const ChatRoom = () => {
                             messages.map((msg, index) => (
                                 <div
                                     key={index}
-                                    className={`flex items-center mb-3 ${msg.username === "Anonyme" ? 'justify-start' : 'justify-end'}`}
+                                    className={`flex items-center mb-3 ${msg.username != username ? 'justify-start' : 'justify-end'}`}
                                 >
                                     <div
-                                        className={`p-3 rounded-lg max-w-md break-words ${msg.username === "Anonyme" ? 'bg-gray-700' : 'bg-gray-700'} text-gray-200`}
+                                        className={`p-3 rounded-lg max-w-md break-words ${msg.username != username ? 'bg-gray-700' : 'bg-gray-700'} text-gray-200`}
                                     >
                                         <span className="font-semibold block mb-1">{msg.username}:</span>
                                         <span>{msg.message}</span>
