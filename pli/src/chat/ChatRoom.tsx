3603264e02ac82879cabb4d46a661d53d08f4725
soler_j <soler_j@etna-alternance.net>
2024-11-05 22:30:48 +0100

    feat(chat): update TURN server configuration with new credentials and endpoints

--- pli/src/chat/ChatRoom.tsx
+++ pli/src/chat/ChatRoom.tsx
@@ -394,18 +394,28 @@ const ChatRoom = () => {
         const peerConnection = new RTCPeerConnection({
             iceServers: [
                 {
-                    urls: "stun:stun.relay.metered.ca:80"
-                },
-                { urls: "stun:stun.l.google.com:19302" },
-                { urls: "stun:stun.l.google.com:5349" },
-                // { urls: "stun:stun1.l.google.com:3478" },
-                // { urls: "stun:stun1.l.google.com:5349" },
-                // { urls: "stun:stun2.l.google.com:19302" },
-                // { urls: "stun:stun2.l.google.com:5349" },
-                // { urls: "stun:stun3.l.google.com:3478" },
-                // { urls: "stun:stun3.l.google.com:5349" },
-                // { urls: "stun:stun4.l.google.com:19302" },
-                // { urls: "stun:stun4.l.google.com:5349" }
+                    urls: "stun:stun.relay.metered.ca:80",
+                  },
+                  {
+                    urls: "turn:europe.relay.metered.ca:80",
+                    username: "8d3c8253cd46b3f0183a2839",
+                    credential: "ugvGPlGuh7A8zVjI",
+                  },
+                  {
+                    urls: "turn:europe.relay.metered.ca:80?transport=tcp",
+                    username: "8d3c8253cd46b3f0183a2839",
+                    credential: "ugvGPlGuh7A8zVjI",
+                  },
+                  {
+                    urls: "turn:europe.relay.metered.ca:443",
+                    username: "8d3c8253cd46b3f0183a2839",
+                    credential: "ugvGPlGuh7A8zVjI",
+                  },
+                  {
+                    urls: "turns:europe.relay.metered.ca:443?transport=tcp",
+                    username: "8d3c8253cd46b3f0183a2839",
+                    credential: "ugvGPlGuh7A8zVjI",
+                  }
             ]
         });
         return peerConnection;
